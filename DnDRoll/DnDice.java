4b9e476593eeef5fa001ee7bbca77e32beddc3ef
dakota <dakota.flath@gmail.com>
2020-01-23 01:53:46 -0800

    Revamping DnDRoll

--- DnDRoll/DnDice.java
+++ DnDRoll/DnDice.java
@@ -3,25 +3,23 @@ import java.util.*;
 public class DnDice {
     Random numgen = new Random();
     private int roll;
-    private String resultString;
+    private String resultString = "";
+    ArrayList <Integer> list = new ArrayList<Integer>();
 
-    public DnDice(int dieType) {
-        roll = numgen.nextInt(dieType) + 1;
-    } public int[] rollArray(int howMany, int dieType) {
-        int[] array = new int[howMany];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = numgen.nextInt(dieType) + 1;
-        } return array;
-    } public String toString() {
-        resultString = String.format("%d", roll);
-        return resultString;
-    } public static void printArray(int[] anArray) {
-        for (int i = 0; i < anArray.length; i++) {
-            if (i > 0) {
-                System.out.print(", ");
-            } System.out.print(anArray[i]);
-        } System.out.print("\n");
-    } public int getRoll() {
-        return roll;
+    public DnDice() {  }
+
+    public ArrayList roll(int howMany, int dieType) {
+      list.clear();
+      for (int i = 0; i < howMany; i++) {
+          roll = numgen.nextInt(dieType) + 1;
+          list.add(roll);
+      } return list;
+    }
+
+    public String toString() {
+      for (int i = 0; i < list.size(); i++) {
+        resultString += list.get(i) + " ";
+      }
+      return resultString;
     }
 }
