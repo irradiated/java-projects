3ed82ddab4f7ff2f1c1474b032fea2b1e720de60
dakota <dakota.flath@gmail.com>
2020-01-24 22:12:23 -0800

    Update DnDRoll.java

--- DnDRoll/DnDRoll.java
+++ DnDRoll/DnDRoll.java
@@ -7,7 +7,7 @@ public class DnDRoll {
     DnDice dice = new DnDice();
 
     System.out.println("Polyhedral Die Roller v1.0 \nWritten by Irradiated");
-    System.out.println("Choose a die [d4, d6, d8, d10, d12, d20], and how many times to roll it, in the format "
+    System.out.println("Choose a die [d4, d6, d8, d10, d12, d20, d100], and how many times to roll it, in the format "
                     + "[3d12, 1d20, etc], or type [exit] to exit the program.\n");
 
     while (true) {
