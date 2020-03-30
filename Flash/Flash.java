01b5460be646aa47dbe89dbaf2e1f0bdfa9169ef
dakota <dakota.flath@gmail.com>
2020-03-30 12:38:57 -0700

    Work on flash

--- Flash/Flash.java
+++ Flash/Flash.java
@@ -1,10 +1,17 @@
 //A project I would like to see become a command line interface flashcard creator, for programming practice and studying
 
-import java.io.*;
+import java.io.*; 
+import java.util.*;
 
 public class Flash {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Flash: CLI flashcard builder");
+        System.out.println("Make a choice (e.g. 1): ");
+        System.out.print("1. View set\n2. Create set");
+        
+        System.out.print("1. View cards\n2. Add card");
     }
 
     private static void fileWriter(String data) {
