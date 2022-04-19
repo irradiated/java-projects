379f371b40da3ee2f0410094270faf9b8049033c
dakota <40772063+irradiated@users.noreply.github.com>
2022-04-19 02:32:09 -0700

    Delete TicTacToe directory

--- TicTacToe/Test.java
+++
@@ -1,6 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        char character = 'a' + 3;
-        System.out.println(character);
-    }
-}
--- TicTacToe/TicTacToe.java
+++
@@ -1,7 +0,0 @@
-import java.util.Random;
-
-public class TicTacToe {
-    public static void main(String[] args) {
-
-    }
-}
